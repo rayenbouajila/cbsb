8c0b2b3f46b6e87a7067c37db50a8c814cacc6a7
rayenbouajila <rayenbouajila19@gmail.com>
2026-07-30 10:41:39 +0100

    add email validator

--- app/auth_utils.py
+++ app/auth_utils.py
@@ -13,7 +13,9 @@ from .database import get_db
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440))
+ACCESS_TOKEN_EXPIRE_MINUTES = ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440")
+)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
